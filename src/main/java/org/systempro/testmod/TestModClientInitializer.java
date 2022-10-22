2eead3fca609d681b2a188625f3a3a86012284a2
Uros <karaleicuu@gmail.com>
2022-10-23 00:07:27 +0200

    default empty mod

---
+++ src/main/java/org/systempro/testmod/TestModClientInitializer.java
@@ -0,0 +1,10 @@
+package org.systempro.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClientInitializer implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/systempro/testmod/TestModInitializer.java
@@ -0,0 +1,10 @@
+package org.systempro.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestModInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
